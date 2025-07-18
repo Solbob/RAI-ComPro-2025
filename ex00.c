53c8eb1d4556830fd5951f77e59d6b75d6c268e5
Solbob <85553606+Solbob@users.noreply.github.com>
2025-07-18 03:28:36 +0000

    actually done

--- ex00.c
+++ ex00.c
@@ -27,10 +27,5 @@ int main(){
     printf("\n%20.3s*", "programming");
     printf("\n%-20.3s*", "programming");
 
-    printf("\nSalary %15s %8d ", ":", 6500);
-    printf("\nMoney/Week %11s %8.2f", ":", 6500.00/7);
-    printf("\nMoney/day %12s %8.3f", ":", 6500.000/4/7);
-
-
     return(0);
 }
